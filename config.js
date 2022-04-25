b773d994b3387fb52bfc85bf353fb3cbc8bc2cd3
thanhconggg <104387296+thanhconggg@users.noreply.github.com>
2022-04-26 01:39:18 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'Hí my darling - Nguyễn Quý Đông !',
+    introDesc: `Ai là cậu bé siêu nhân 3s cụa anh làoooo`,
+    btnIntro: 'Chọn vào đây đi ',
+    title: 'Bé có thích Nguyễn Thành Công không??',
+    desc: 'Trả lờiiiii ',
+    btnYes: 'Thích ',
+    btnNo: 'Không thích',
+    question:'Sao em thức khuya vậy? Em thich buoi toi a?',
+    btnReply: 'Send Nudes cho anhhhh nhaaaa <3',
+    reply: 'Yêu Nguyễn Thành Công siêu nhiều <33333333',
+    mess: 'Anh biết mà 🥰. Yêu Đông nhiều nhiều 😘😘',
+    messDesc: 'Khi nào bé trả bài cho anh nhá',
+    btnAccept: 'Oke nà <3',
+    messLink: 'https://www.facebook.com/profile.php?id=100026010852096' 
+}
